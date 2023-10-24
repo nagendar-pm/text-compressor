408b62c5502bd65d3a68889015755a400989d8ec
pagidimarri_nagendar <pagidimarri.nagendar@paytmpayments.com>
2023-10-24 20:01:24 +0530

    setting up local repo - initial ide commit

---
+++ src/main/java/com/nagendar/learning/Main.java
@@ -0,0 +1,12 @@
+package com.nagendar.learning;
+
+/*
+ * @author: pagidimarri.nagendar
+ * @createdAt: 24/10/23 7:56 pm
+ */
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
